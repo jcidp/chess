b668bb2b10a82c353fc85f3097b6b8e0379013c5
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-17 17:17:28 -0600

    Add queen path and valid_move?

--- lib/piece/queen.rb
+++ lib/piece/queen.rb
@@ -5,9 +5,21 @@ class Queen
   attr_reader :code, :color, :type
 
   def valid_move?(from, to)
+    row_abs_diff = (to[0] - from[0]).abs
+    col_abs_diff = (to[1] - from[1]).abs
+    row_abs_diff == col_abs_diff || row_abs_diff.zero? || col_abs_diff.zero?
   end
 
   def path(from, to)
+    result = []
+    row_sign, col_sign, fixed_row, fixed_col = signs_and_fixed(from, to)
+    current_square = from.dup
+    while current_square != to
+      current_square[0] = current_square[0].send(row_sign, 1) unless fixed_row
+      current_square[1] = current_square[1].send(col_sign, 1) unless fixed_col
+      result << current_square.dup unless current_square == to
+    end
+    result
   end
 
   private
@@ -18,5 +30,15 @@ class Queen
     self.color = color
     self.type = "queen"
     self.code = color == "white" ? "\u2655" : "\u265B"
+  end
+
+  def signs_and_fixed(from, to)
+    row_diff = to[0] - from[0]
+    col_diff = to[1] - from[1]
+    row_sign = row_diff.positive? ? :+ : :-
+    col_sign = col_diff.positive? ? :+ : :-
+    fixed_row = row_diff.zero?
+    fixed_col = col_diff.zero?
+    [row_sign, col_sign, fixed_row, fixed_col]
   end
 end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require "./lib/piece/queen"
+
+describe Queen do
+  subject(:test_queen) { described_class.new("white") }
+
+  describe "#valid_move?" do
+    it "returns true from 2,2 to 2,0" do
+      from = [2, 2]
+      to = [2, 0]
+      result = test_queen.valid_move?(from, to)
+      expect(result).to be true
+    end
+
+    it "returns true from 7,5 to 1,5" do
+      from = [7, 5]
+      to = [1, 5]
+      result = test_queen.valid_move?(from, to)
+      expect(result).to be true
+    end
+
+    it "returns true from 0,2 to 5,7" do
+      from = [0, 2]
+      to = [5, 7]
+      result = test_queen.valid_move?(from, to)
+      expect(result).to be true
+    end
+
+    it "returns true from 7,5 to 6,4" do
+      from = [7, 5]
+      to = [6, 4]
+      result = test_queen.valid_move?(from, to)
+      expect(result).to be true
+    end
+
+    it "returns false from 7,6 to 6,4" do
+      from = [7, 6]
+      to = [6, 4]
+      result = test_queen.valid_move?(from, to)
+      expect(result).to be false
+    end
+  end
+
+  describe "#path" do
+    context "when moving from 3,0 to 3,7" do
+      it "returns the correct horizontal path" do
+        from = [3, 0]
+        to = [3, 7]
+        result = test_queen.path(from, to)
+        expect(result).to eql([[3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6]])
+      end
+    end
+
+    context "when moving from 7,5 to 0,5" do
+      it "returns the correct vertical path" do
+        from = [7, 5]
+        to = [0, 5]
+        result = test_queen.path(from, to)
+        expect(result).to eql([[6, 5], [5, 5], [4, 5], [3, 5], [2, 5], [1, 5]])
+      end
+    end
+
+    context "when moving from 4,6 to 4,5" do
+      it "returns an empty path" do
+        from = [4, 6]
+        to = [4, 5]
+        result = test_queen.path(from, to)
+        expect(result).to eql([])
+      end
+    end
+
+    context "when moving from 0,2 to 5,7" do
+      it "returns the correct diagonal path" do
+        from = [0, 2]
+        to = [5, 7]
+        result = test_queen.path(from, to)
+        expect(result).to eql([[1, 3], [2, 4], [3, 5], [4, 6]])
+      end
+    end
+
+    context "when moving from 7,2 to 4,5" do
+      it "returns the correct diagonal path" do
+        from = [7, 2]
+        to = [4, 5]
+        result = test_queen.path(from, to)
+        expect(result).to eql([[6, 3], [5, 4]])
+      end
+    end
+  end
+end
