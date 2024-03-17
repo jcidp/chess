1f0cc24b3b7d6bd545884d60bdfdf9dbaf203c2b
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-17 16:48:30 -0600

    Add rook path and valid_move?

--- lib/piece/rook.rb
+++ lib/piece/rook.rb
@@ -5,9 +5,21 @@ class Rook
   attr_reader :code, :color, :type
 
   def valid_move?(from, to)
+    to[0] == from[0] || to[1] == from[1]
   end
 
   def path(from, to)
+    result = []
+    row_sign = (to[0] - from[0]).positive? ? :+ : :-
+    col_sign = (to[1] - from[1]).positive? ? :+ : :-
+    direction = to[0] == from[0] ? "col" : "row"
+    current_square = from.dup
+    while current_square != to
+      current_square[0] = current_square[0].send(row_sign, 1) if direction == "row"
+      current_square[1] = current_square[1].send(col_sign, 1) if direction == "col"
+      result << current_square.dup unless current_square == to
+    end
+    result
   end
 
   private
