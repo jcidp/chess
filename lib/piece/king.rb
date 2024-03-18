96a980a4ea922a0fd8b2651566b0c9975f7d203c
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-17 20:05:32 -0600

    Add king path and move_valid?

--- lib/piece/king.rb
+++ lib/piece/king.rb
@@ -2,12 +2,17 @@
 
 # Represents a King piece
 class King
+  attr_accessor :unmoved
   attr_reader :code, :color, :type
 
   def valid_move?(from, to)
+    row_abs_diff = (to[0] - from[0]).abs
+    col_abs_diff = (to[1] - from[1]).abs
+    row_abs_diff.between?(0, 1) && col_abs_diff.between?(0, 1)
   end
 
-  def path(from, to)
+  def path(*)
+    []
   end
 
   private
